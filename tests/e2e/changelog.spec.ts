7202d40c8ac67bd61d1afa77bc72e6c51c53b370
Gabriel Kanev <contact@mrgkanev.eu>
2026-08-26 14:40:45 +0300

    moving things around

---
+++ tests/e2e/changelog.spec.ts
@@ -0,0 +1,14 @@
+import { expect, test } from '@playwright/test';
+
+test('release notes render Markdown as formatted HTML', async ({ page }) => {
+  await page.goto('/changelog/');
+
+  const firstRelease = page.locator('article').first();
+  await expect(firstRelease.locator('.release-notes strong').first()).toHaveText('Full Changelog');
+  await expect(firstRelease.locator('.release-notes a').first()).toHaveAttribute('href', /^https:\/\/github\.com\//);
+  await expect(firstRelease).not.toContainText('**Full Changelog**');
+
+  const detailedRelease = page.locator('article').filter({ hasText: 'multi-store-sync-for-woocommerce' });
+  await expect(detailedRelease.locator('.release-notes h2')).toContainText('WooCommerce Multi-Store Sync');
+  await expect(detailedRelease.locator('.release-notes li')).not.toHaveCount(0);
+});
